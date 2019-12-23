5c97b588d50b1c476e1a8d8b1a2e1e82cd829b44
angshuai <529347062@qq.com>
2019-12-23 12:19:04 +0800

    angshuai

---
+++ cashLoans-user/src/main/java/aobai/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package aobai.user.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author angshuai
+ * @Date 2019/12/12 13:14
+ * @Version 1.0
+ **/
+@Api("UserController|用户信息相关")
+@RequestMapping("/user")
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+    @ApiOperation(value = "用户信息列表")
+    @RequestMapping("/inform")
+    public List<User> inform(){
+        return userService.selectList();
+    }
+}
---
+++ cashLoans-user/src/test/java/aobai/user/CashLoadsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package aobai.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashLoadsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
