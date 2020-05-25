88ae6fbb4d92873b254fd229898c6a4d1e764039
angshuai <529347062@qq.com>
2020-05-25 16:22:06 +0800

    angshuai

--- cashLoans-account/src/main/java/aobai/account/CashLoadsAccountApplication.java
+++ cashLoans-account/src/main/java/aobai/account/CashLoadsAccountApplication.java
@@ -1,12 +1,9 @@
 package aobai.account;
 
-//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class CashLoadsAccountApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CashLoadsAccountApplication.class, args);
--- cashLoans-contract/src/main/java/aobai/contract/CashLoadsContractApplication.java
+++ cashLoans-contract/src/main/java/aobai/contract/CashLoadsContractApplication.java
@@ -3,11 +3,8 @@ package aobai.contract;
 //import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 //@MapperScan(basePackages={"com.aobai.user.mapper"})
-@EnableSwagger2
 public class CashLoadsContractApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CashLoadsContractApplication.class, args);
