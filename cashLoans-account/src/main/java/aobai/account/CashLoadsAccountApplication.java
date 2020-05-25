14aa47b92ac21894d90940784d53f0b4efacee7a
angshuai <529347062@qq.com>
2020-05-25 10:27:14 +0800

    angshuai

--- cashLoans-account/src/main/java/aobai/account/CashLoadsAccountApplication.java
+++ cashLoans-account/src/main/java/aobai/account/CashLoadsAccountApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@MapperScan(basePackages={"com.aobai.user.mapper"})
 @EnableSwagger2
 public class CashLoadsAccountApplication {
 	public static void main(String[] args) {
