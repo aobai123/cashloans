01dc3d05a3567dd710496b55af26b4f39692d47e
angshuai <529347062@qq.com>
2020-05-25 14:49:33 +0800

    angshuai

--- leaf-parent/leaf-core/src/test/java/com/aobai/leafcore/LeafCoreApplicationTests.java
+++ leaf-parent/leaf-core/src/test/java/com/aobai/leafcore/LeafCoreApplicationTests.java
@@ -1,13 +1,6 @@
 package com.aobai.leafcore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class LeafCoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
